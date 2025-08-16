4f38b5614604616180f6fbe73a7464c3b5d2e810
Steven Dahdah <sdahdah@users.noreply.github.com>
2025-08-16 10:41:57 -0400

    Add minimal options

--- nvim/dot-config/nvim/init.lua
+++ nvim/dot-config/nvim/init.lua
@@ -4,5 +4,26 @@ require "paq" {
     {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
 }
 
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.colorcolumn = '80'
+vim.opt.signcolumn = 'yes'
+vim.opt.cursorline = true
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.list = true
+vim.opt.listchars:append({
+    nbsp = "¬",
+    tab = "| ",
+    extends = "»",
+    precedes = "«",
+    trail = "·",
+})
+vim.opt.showbreak = "↳ "
+vim.opt.wrap = false
+vim.opt.linebreak = true
 vim.opt.termguicolors = true
 vim.cmd.colorscheme "falcon"
